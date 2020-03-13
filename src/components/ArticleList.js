ca4dce41287625fea6313640d80170637204cbc8
ZSoftwareDevelopment <zsoftwaredevelopment@gmail.com>
2020-03-13 13:52:30 +0200

    HW2

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,25 +1,31 @@
 import React, { Component } from "react";
 import Article from "./Article";
+import PropTypes from "prop-types";
+import accordion from "../decorators/accordion";
 
-export default class ArticleList extends Component {
-  state = {
-    openArticleId: null
+class ArticleList extends Component {
+  static propTypes = {
+    articles: PropTypes.array.isRequired,
+    //from accordion
+    openItemId: PropTypes.string,
+    toggleOpenItem: PropTypes.func.isRequired
   };
+
   render() {
-    const articleElements = this.props.articles.map(article => (
+    const { articles, openItemId, toggleOpenItem } = this.props;
+
+    const articleElements = articles.map(article => (
       <li key={article.id}>
         <Article
           article={article}
-          isOpen={article.id === this.state.openArticleId}
-          toggleOpen={this.toggleOpenArticle(article.id)}
+          isOpen={article.id === openItemId}
+          toggleOpen={toggleOpenItem(article.id)}
         />
       </li>
     ));
 
     return <ul>{articleElements}</ul>;
   }
-
-  toggleOpenArticle = openArticleId => ev => {
-    this.setState({ openArticleId });
-  };
 }
+
+export default accordion(ArticleList);
--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import PropTypes from "prop-types";
 import Comment from "./Comment";
 import toggleOpen from "../decorators/toggleOpen";
 
@@ -12,6 +12,13 @@ function CommentList({ comments = [], isOpen, toggleOpen }) {
     </div>
   );
 }
+
+CommentList.propTypes = {
+  comments: PropTypes.array,
+  //from toggleOpen decorator
+  isOpen: PropTypes.bool,
+  toggleOpen: PropTypes.func
+};
 
 function getBody({ comments, isOpen }) {
   if (!isOpen) return null;
