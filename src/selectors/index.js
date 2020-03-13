7440f5792a2c4089aebdc420ae0b5102fbaff198
ZSoftwareDevelopment <zsoftwaredevelopment@gmail.com>
2020-03-14 00:25:11 +0200

    how to get comment by id

--- src/selectors/index.js
+++ src/selectors/index.js
@@ -29,5 +29,5 @@ export const filtratedArticlesSelector = createSelector(
 export const commentSelectorFactory = () =>
   createSelector(commentsGetter, idGetter, (comments, id) => {
     console.log("commentSelector")
-    return comments.find(comment => comment.id === id)
+    return comments[id]
   })
